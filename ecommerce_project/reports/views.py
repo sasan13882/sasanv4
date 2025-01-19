33b3ff6d1b116a34df343c6098052dd47ffbc783
sasan13882 <hadiebrahimiebi13@gmail.com>
2025-01-19 08:40:33 +0330

    Update views.py

--- ecommerce_project/reports/views.py
+++ ecommerce_project/reports/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import SalesReport
 
-# Create your views here.
+def generate_sales_report(request):
+    date = request.GET.get('date')
+    report = SalesReport.generate_report(date)
+    return render(request, 'reports/sales_report.html', {'report': report})
