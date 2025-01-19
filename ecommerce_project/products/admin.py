9dcc1d1b8872ce25c98ed4e15a04509fad3ddc35
sasan13882 <hadiebrahimiebi13@gmail.com>
2025-01-19 08:39:19 +0330

    Update admin.py

--- ecommerce_project/products/admin.py
+++ ecommerce_project/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['code', 'description', 'price_rial', 'category']
+    search_fields = ['code', 'description']
+    list_filter = ['category']
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
