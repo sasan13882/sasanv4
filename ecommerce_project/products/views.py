d092841a1604a977a8045f94f17779779dbea1ee
sasan13882 <hadiebrahimiebi13@gmail.com>
2025-01-19 08:39:36 +0330

    Update views.py

--- ecommerce_project/products/views.py
+++ ecommerce_project/products/views.py
@@ -1,6 +1,10 @@
-# products/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-from django.http import HttpResponse
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the products index.")
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
