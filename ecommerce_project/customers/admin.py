8698f83e74f1504ae94f648c89525b94a7f62b8d
sasan13882 <hadiebrahimiebi13@gmail.com>
2025-01-19 08:37:18 +0330

    Update admin.py

--- ecommerce_project/customers/admin.py
+++ ecommerce_project/customers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'created_at', 'updated_at', 'is_staff', 'is_active']
+    search_fields = ['email', 'username']
+
+admin.site.register(User, UserAdmin)
