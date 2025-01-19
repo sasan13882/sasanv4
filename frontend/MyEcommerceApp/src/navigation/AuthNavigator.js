84386d6d64db3691dad66a8a9d98ffc632a42774
sasan13882 <hadiebrahimiebi13@gmail.com>
2025-01-19 09:35:02 +0330

    Update AuthNavigator.js

---
+++ frontend/MyEcommerceApp/src/navigation/AuthNavigator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+
+const Stack = createStackNavigator();
+
+export default function AuthNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
